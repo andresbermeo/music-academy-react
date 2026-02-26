16fa0c08ce2f99868a5b5d1ffc09c159a70f7fa8
soy Andres Bermeo <andresillo7@gmail.com>
2026-02-26 18:27:17 -0500

    Initial commit: Music Academy in React

---
+++ mi-proyecto-react/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <main>
+      <nav>
+        <h2>Music Academy</h2>
+      </nav>
+
+      <section>
+        <h1>Learn Music from Home</h1>
+        <p>Master your favorite instrument with our online expert-led courses.</p>
+        <button>Explore Courses</button>
+      </section>
+    </main>
+  )
+}
+
+export default App
